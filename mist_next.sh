707ef0cdcbc4c5c65ac854bb6ad92d57ec34bf0e
akashrajr1 <akashrajr1@gmail.com>
2017-10-23 02:51:05 +0530

    Late night work

--- mist_next.sh
+++ mist_next.sh
@@ -9,8 +9,20 @@ clear
 ./rr.sh page3
 read -s next
 tput setaf 2
-cat anonymous | nms -f green
+cat anonymous | nms -asf green
 read -s next
 clear
 ./rr.sh page4
 read -s next
+clear
+./rr.sh page5
+cat logo_high_res | nms -f green
+clear
+echo -n "Entering standby mode"
+for i in $(seq 5); do
+	echo -n .
+	sleep .7
+done
+sleep 3
+clear
+./matrix.sh
